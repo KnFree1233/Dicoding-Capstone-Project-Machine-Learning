17ad636165535e8ff655dcd3c94de15c1eb574fb
Siddi Ardhana <siddiardhana@gmail.com>
2025-10-19 20:31:01 +0700

    Change How to Calculate Food Height and Area

--- food_nutrition_api/calculate_food_size.py
+++ food_nutrition_api/calculate_food_size.py
@@ -1,3 +1,4 @@
+import numpy as np
 from tools import get_size_px, get_median_depth
 
 FOOD_DENSITY = 1
@@ -5,20 +6,21 @@ FOOD_DENSITY = 1
 def calculate_food_size(food_results, depth_map, size_cm_per_px):
     real_food_sizes = []
     for mask in food_results['resize_food_masks']:
-        object_size = get_size_px(mask)
-        object_depth = get_median_depth(mask, depth_map)
-        object_size["depth"] = object_depth
+        # Calculate food height
+        mask_depth = depth_map[mask > 0]
+        depth_value_top = np.min(mask_depth)
+        depth_value_bottom = np.max(mask_depth)
+        object_height = abs(depth_value_bottom - depth_value_top)
 
-        food_length = object_size["length"] * size_cm_per_px["cm_per_px_length"]
-        food_width = object_size["width"] * size_cm_per_px["cm_per_px_width"]
-        food_depth = object_size["depth"] * size_cm_per_px["cm_per_depth_unit"]
-        food_area = food_length * food_width
-        food_volume = food_length * food_width * food_depth
+        # Get pixel area from mask
+        object_pixel_area = np.count_nonzero(mask)
+
+        food_height = object_height * size_cm_per_px["cm_per_depth_unit"]
+        food_area = object_pixel_area * (size_cm_per_px["cm_per_px_length"] ** 2)
+        food_volume = food_area * food_height
         food_weight = food_volume * FOOD_DENSITY
         real_food_sizes.append({
-            "length": food_length,
-            "width": food_width,
-            "depth": food_depth,
+            "height": food_height,
             "area": food_area,
             "volume": food_volume,
             "weight": food_weight,
